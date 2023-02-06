2986c27bed42f1d4df5626fbb02fa9730836157a
Morning <morning@admins-iMac.local>
2023-02-06 12:21:56 +0800

    header width

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,16 +4,20 @@ import HeaderIcon from "./icons/HeaderIcon";
 import LogoutIcon from "./icons/LogoutIcon";
 
 export default function Header() {
-  return <div className="header">
-    <div className="container">
-      <HeaderIcon/>
-      <div className="search-input">
-        <input type="search" placeholder="Search" />
-        <button className="search-button">Search</button>
+  return (
+    <div className="header">
+      <div className="container">
+        <HeaderIcon />
+
+        <div className="search-input">
+          <input type="search" placeholder="Search" />
+          <button className="search-button">Search</button>
+        </div>
+        <button className="logout">
+          <LogoutIcon />
+          Гарах
+        </button>
       </div>
-      <button className="logout">
-        <LogoutIcon/>
-        Гарах</button>
     </div>
-  </div>;
+  );
 }
